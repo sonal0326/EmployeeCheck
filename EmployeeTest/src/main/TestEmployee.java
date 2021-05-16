796eb98b0d60a50c00945d1b9d2f4070cdd2dbff
Yogendra Sahu <yogendra.sahu@viacomcontractor.com>
2021-05-17 00:10:49 +0530

    Addedd employee ID

--- EmployeeTest/src/main/TestEmployee.java
+++ EmployeeTest/src/main/TestEmployee.java
@@ -10,8 +10,10 @@ public class TestEmployee {
 		emp1.setName("Sonal");
 		emp1.setAge(21);
 		emp1.setAdd("pune");
+		emp1.setEmpId(101);
 		System.out.println("Get Employee 1 details:");
 		System.out.println("Employee Name: " + emp1.getName());
 		System.out.println("Employee address : " + emp1.getAdd());
+		System.out.println("Employee ID: " + emp1.getEmpId());
 	}
 }
